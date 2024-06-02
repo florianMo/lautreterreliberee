bc47e4933b982ed322852366d6dd710cd89aeade
Florian Motteau <florian.motteau@nereide.fr>
2024-06-02 22:45:21 +0200

    modif libellé podcast

--- src/routes/(app)/balade-sonore/+page.svelte
+++ src/routes/(app)/balade-sonore/+page.svelte
@@ -5,9 +5,9 @@
 <h1 class="text-center frankie text-4xl mb-4">Balade sonore</h1>
 <h2 class="text-center frankie text-2xl mb-4">En chemin vers «Terre Libérée»</h2>
 
-<p class="text-lg my-2 font-bold text-5xl">Départ et arrivée : les halles de Luynes</p>
+<p class="my-2 font-bold text-2xl">Départ et arrivée : les halles de Luynes</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/1">Etape 1</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/1">Podcast étape 1</a></p>
 
 <iframe
         id="map"
@@ -22,29 +22,29 @@
 
 <p class="text-lg my-2">Longer le cimetière et pénétrer par l’entrée au-dessus à droite. Emprunter la 2e allée à gauche ; la tombe de Louis Rimbault est la 8e à droite.</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/2">Etape 2</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/2">Podcast étape 2</a></p>
 
 <p class="text-lg my-2">Sortir du cimetière et reprendre la montée raide mais courte qui longe bientôt un vignoble avec une loge de vigne remarquable. A 250 m. environ à gauche, un panneau apporte quelques informations sur le château de Luynes. Dépasser le Moulin Doré et longer des champs sur une centaine de mètres avant de quitter la route qui descend pour tourner à gauche à angle droit sur un chemin en bordure de haie.</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/3">Etape 3</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/3">Podcast étape 3</a></p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/4">Etape 4</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/4">Podcast étape 4</a></p>
 
 <p class="text-lg my-2">Rester sur ce chemin qui traverse haies variées, champs, prés en offrant un beau point de vue vers l’ouest avant de descendre vers la route. Tourner à droite, longer un rucher sur la droite et poursuivre vers le Moulin Hodoux. Dépasser Les Planches, continuer par la voie sans issue pour rejoindre le moulin et prendre de suite un étroit chemin à gauche, entre deux piliers, s’enfonçant sous les arbres.</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/5">Etape 5</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/5">Podcast étape 5</a></p>
 
 <p class="text-lg my-2">Traverser 3 passerelles puis longer un ruisseau à droite avant de parvenir rapidement à la route. Tourner à droite puis, au panneau Le Vieux Bourg, poursuivre sur le chemin à droite (balisage jaune).</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Etape 6</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Podcast étape 6</a></p>
 
 <p class="text-lg my-2">Au bout de ce chemin, prendre à droite au-dessus d’un petit pont, suivi de deux autres, puis tourner à gauche sur un bon chemin le long d’un pré à chevaux. Après un virage à droite emprunter, avant la route, un chemin en herbe à droite en direction d’un bois. S’enfoncer dans les sous-bois par une belle montée ombragée. Dépasser une grande propriété clôturée puis, avant une petite clairière, prendre à gauche un sentier descendant bien marqué le long d’un fossé à droite jusqu’à la route.</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Etape 7</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Podcast étape 7</a></p>
 
 <p class="text-lg my-2">Prendre à droite sur 300m environ et, 50m après l’arrêt de bus, tourner à gauche vers le hameau Le Pin (panneau lisible de l’autre côté). Monter jusqu’en haut de la butte pour observer le paysage de « Terre Libérée » (maison et terrains). Redescendre quelques dizaines de mètres jusqu’à l’entrée à gauche d’un terrain géré par l’association « L’Autre Terre Libérée » (pause et pique-nique possibles).</p>
 
-<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Etape 8</a></p>
+<p class="text-2xl"><a class="frankie" href="/balade-sonore/6">Podcast étape 8</a></p>
 
 <p class="text-lg my-2">Quitter le terrain pour redescendre et prendre, juste après celui-ci, un chemin plus ou moins fauché qui traverse champs et friches vers Luynes, devenant carrossable entre de belles haies où la concurrence des chants d’oiseaux bat son plein.</p>
 
